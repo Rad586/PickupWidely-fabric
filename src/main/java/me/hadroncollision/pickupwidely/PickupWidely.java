3e235e421aef51451be262bb95a3a820c1d8a144
BuildTools <unconfigured@null.spigotmc.org>
2023-05-17 03:53:04 +0500

    nothing

--- src/main/java/me/hadroncollision/pickupwidely/PickupWidely.java
+++ src/main/java/me/hadroncollision/pickupwidely/PickupWidely.java
@@ -2,13 +2,13 @@ package me.hadroncollision.pickupwidely;
 
 import me.hadroncollision.pickupwidely.config.ModConfigs;
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import java.util.logging.Logger;
 
 public class PickupWidely implements ModInitializer
 {
 	public static final String MOD_ID = "pickupwidely";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Logger LOGGER = Logger.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize()
